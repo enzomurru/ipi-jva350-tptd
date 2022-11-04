a91a66d460f3a2c6ca1ce6b3db851fb023c4b694
enzomurru <enzomurru@leschassagnes.net>
2022-11-04 09:39:29 +0100

    test Test

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -1,11 +1,31 @@
 package com.ipi.jva350.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SalarieAideADomicileTest {
     @Test
-    void aLegalementDroitADesCongesPayes() {
+    void aLegalementDroitADesCongesPayesTrue() {
+        //Given
+        SalarieAideADomicile aide = new SalarieAideADomicile();
+            //LocalDate.now(), LocalDate.now(), 0, 0;
+        //When
+        boolean res = aide.aLegalementDroitADesCongesPayes();
+        //Then
+        Assertions.assertEquals(1,res);
+    }
+
+    @Test
+    void aLegalementDroitADesCongesPayesFalse() {
+        //Given
+        SalarieAideADomicile aide = new SalarieAideADomicile();
+        //When
+        boolean res = aide.aLegalementDroitADesCongesPayes();
+        //Then
+        Assertions.assertEquals(0,res);
     }
 }
