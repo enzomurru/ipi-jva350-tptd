4d0b4c4388a12a58d1846f7e575ff9e54dcfba22
enzomurru <enzomurru@leschassagnes.net>
2022-11-04 11:54:39 +0100

    Test Methode ok

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -9,12 +9,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SalarieAideADomicileTest {
     @Test
-    void aLegalementDroitADesCongesPayesTrue() {
+    void aLegalementDroitADesCongesPayesMoinsDe10() {
         //Given
-        SalarieAideADomicile aide = new SalarieAideADomicile();
+        SalarieAideADomicile aide = new SalarieAideADomicile("Jeanne",
+                LocalDate.now(), LocalDate.now(), 0, 0, 5, 1, 0);
         //When
         boolean res = aide.aLegalementDroitADesCongesPayes();
         //Then
+        Assertions.assertEquals(false, res);
+    }
+    @Test
+    void aLegalementDroitADesCongesPayesCasAuxLimites9() {
+        //Given
+        SalarieAideADomicile aide = new SalarieAideADomicile("Jeanne",
+                LocalDate.now(), LocalDate.now(), 0, 0, 9, 1, 0);
+        //When
+        boolean res = aide.aLegalementDroitADesCongesPayes();
+        //Then
+        Assertions.assertEquals(false, res);
+
+        //Given
+        aide.setJoursTravaillesAnneeNMoins1(10);
+        //When, Then
+        res = aide.aLegalementDroitADesCongesPayes();
         Assertions.assertEquals(true, res);
     }
 
