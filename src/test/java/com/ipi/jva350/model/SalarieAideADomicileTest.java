95f2d5b3d0ec0618c2b1454c8be1f0e40a278bc2
enzomurru <enzomurru@leschassagnes.net>
2022-11-30 14:17:52 +0100

    Test Methode ok

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -1,13 +1,39 @@
 package com.ipi.jva350.model;
 
+import io.cucumber.java.en.When;
+import jdk.vm.ci.meta.Local;
+import org.assertj.core.api.Java6StandardSoftAssertionsProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SalarieAideADomicileTest {
+
+    private Java6StandardSoftAssertionsProvider Assertion;
+    @ParameterizedTest(name = "entre {0} et {1}")
+    @CsvSource({
+            "XXXXXXXXXX, 2"
+    })
+    void calculeJoursDeCongeDecomptesPourPlage() {
+        //Given
+        SalarieAideADomicile salarie = new SalarieAideADomicile("Jeanne", LocalDate.of(2021, 7, 1),
+                LocalDate.now(), 0, 0, 9, 1, 0);
+        //Then
+        LinkedHashSet<LocalDate> res = salarie.calculeJoursDeCongeDecomptesPourPlage(LocalDate.of(2021, 7, 1),
+                LocalDate.of(2022, 7, 27));
+        //When
+        LinkedHashSet<LocalDate> expected = new LinkedHashSet<>();
+        expected.add(LocalDate.of(2022, 7, 11));
+        expected.add(LocalDate.parse("2022-07-11"));
+        Assertions.assertEquals(false, res);
+    }
+
     @Test
     void aLegalementDroitADesCongesPayesMoinsDe10() {
         //Given
